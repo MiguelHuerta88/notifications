c36e85b8ab23d62055af427d88ca7e3e7e347932
Miguel Huerta <miguel.huerta@internetbrands.com>
2018-05-22 09:52:17 -0700

    Saving Point. going to rebuild VM with php 7

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::group(['prefix' => 'api'], function(){
+    Route::post('create', function(){
+        dd('here');
+    });
+
+    Route::post('update', function(){
+        dd('here');
+    });
 });
