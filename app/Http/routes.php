04dd7d32cd430aa1817794e30af382664d5fb2cc
Miguel Huerta <miguel.huerta@internetbrands.com>
2018-05-22 12:48:23 -0700

    New Vm using PHP7

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
 Route::group(['prefix' => 'api'], function(){
     Route::post('create', function(){
