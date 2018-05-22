62221f94d527e9a9d49a0522827cba4ce54d2e0c
Miguel Huerta <miguel.huerta@internetbrands.com>
2018-05-22 13:44:32 -0700

    saving point.

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miguelhuerta
+ * Date: 5/22/18
+ * Time: 1:30 PM
+ */
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+
+class SubscriptionController extends Controller
+{
+    public function __construct()
+    {
+        // set up anything
+    }
+}
---
+++ app/Models/Subscriptions.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Created by PhpStorm.
+ * User: miguelhuerta
+ * Date: 5/22/18
+ * Time: 1:42 PM
+ */
+class Subscriptions extends Model
+{
+
+}
